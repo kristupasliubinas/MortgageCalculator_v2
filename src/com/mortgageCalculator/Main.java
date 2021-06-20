f5102cca6ffeec56dea139869964e1c1046446ba
kristupas <kristupas.liubinas@gmail.com>
2021-06-20 19:42:59 +0100

    Input validation - version 1

--- src/com/mortgageCalculator/Main.java
+++ src/com/mortgageCalculator/Main.java
@@ -12,14 +12,29 @@ public class Main {
         // Taking input from the user
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Principal: ");
+        System.out.print("Principal (£1K - £1M): ");
         int principal = scanner.nextInt();
+        while (!(principal >= 1000 && principal <= 1_000_000)) {
+            System.out.println("Enter a number between 1,000 and 1,000,000.");
+            System.out.print("Principal (£1K - £1M): ");
+            principal = scanner.nextInt();
+        }
 
         System.out.print("Annual Interest Rate: ");
         float annualInterest = scanner.nextFloat();
+        while (!(annualInterest > 0 && annualInterest <= 30)) {
+            System.out.println("Enter a value greater than 0 and less than or equal to 30.");
+            System.out.print("Annual Interest Rate: ");
+            annualInterest = scanner.nextFloat();
+        }
 
         System.out.print("Period (Years): ");
         byte years = scanner.nextByte();
+        while (!(years >= 1 && years <= 30)) {
+            System.out.println("Enter a value between 1 and 30.");
+            System.out.print("Period (Years): ");
+            years = scanner.nextByte();
+        }
 
 
         // Mortgage calculation
